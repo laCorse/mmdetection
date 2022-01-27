65fe08b3022d4eab49af278ec273c25df1c36349
PJLAB\huanghaian <1286304229@qq.com>
2022-01-27 19:02:03 +0800

    add 90k config

---
+++ configs/cascade_rcnn/cascade_mask_rcnn_r50_caffe_fpn_mstrain-poly90k_coco.py
@@ -0,0 +1,42 @@
+_base_ = ['./cascade_mask_rcnn_r50_caffe_fpn_1x_coco.py']
+
+img_norm_cfg = dict(
+    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadAnnotations',
+        with_bbox=True,
+        with_mask=True,
+        poly2mask=False),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+
+data = dict(
+    train=dict(pipeline=train_pipeline))
+
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=0.001,
+    step=[60000, 80000])
+
+# Runner type
+runner = dict(_delete_=True, type='IterBasedRunner', max_iters=90000)
+
+checkpoint_config = dict(interval=10000)
+evaluation = dict(interval=10000)
+
+log_config = dict(interval=20)
