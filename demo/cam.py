2703cced9864ef767da0ee0cb4afd5738027b30a
hha <1286304229@qq.com>
2022-05-14 11:17:10 +0800

    update

--- demo/cam.py
+++ demo/cam.py
@@ -1,37 +1,176 @@
+import argparse
+import os.path
+from functools import partial
+
 import cv2
-from pytorch_grad_cam import AblationCAM
-import torch
-from mmdet.utils.det_cam import CAMWrapperModel, DetBoxScoreTarget, DetCam
+import mmcv
+import numpy as np
+from mmcv import Config, DictAction
+
+from mmdet.utils.det_cam import (AblationLayerBackboneOrNeck, CAMWrapperModel,
+                                 DetBoxScoreTarget, DetCam,
+                                 backbone_or_neck_reshape_transform)
+
+try:
+    from pytorch_grad_cam import AblationCAM, EigenCAM
+except ImportError:
+    raise ImportError('Please run `pip install "grad-cam"` to install '
+                      '3rd party package pytorch_grad_cam.')
+
+METHOD_MAP = {'ablationcam': AblationCAM, 'eigencam': EigenCAM}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Visualize CAM')
+    parser.add_argument('img', help='Image file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument(
+        '--method',
+        default='ablationcam',
+        help='Type of method to use, supports '
+        f'{", ".join(list(METHOD_MAP.keys()))}.')
+    parser.add_argument(
+        '--target-layers',
+        default=['neck'],
+        nargs='+',
+        type=str,
+        help='The target layers to get CAM, if not set, the tool will '
+        'specify the backbone layer3')
+    parser.add_argument(
+        '--preview-model',
+        default=False,
+        action='store_true',
+        help='To preview all the model layers')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--score-thr', type=float, default=0.3, help='bbox score threshold')
+    parser.add_argument(
+        '--topk',
+        type=int,
+        default=2,
+        help='topk of the predicted result to visualizer')
+    parser.add_argument(
+        '--batch', type=int, default=1, help='batch of inference')
+    parser.add_argument(
+        '--ratio-channels-to-ablate',
+        type=int,
+        default=0.1,
+        help='making it much faster of AblationCAM')
+    parser.add_argument(
+        '--max-reshape-shape',
+        type=tuple,
+        default=(20, 20),
+        help='max reshape shapes of AblationCAM')
+    parser.add_argument(
+        '--aug-smooth',
+        default=False,
+        action='store_true',
+        help='Wether to use test time augmentation, default not to use')
+    parser.add_argument(
+        '--eigen-smooth',
+        default=False,
+        action='store_true',
+        help='Reduce noise by taking the first principle componenet of '
+        '``cam_weights*activations``')
+    parser.add_argument('--out-dir', default=None, help='dir to output file')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
+    args = parser.parse_args()
+    if args.method.lower() not in METHOD_MAP.keys():
+        raise ValueError(f'invalid CAM type {args.method},'
+                         f' supports {", ".join(list(METHOD_MAP.keys()))}.')
+
+    return args
+
+
+def init_mode_cam(args, cfg):
+    # build the model from a config file and a checkpoint file
+    model = CAMWrapperModel(
+        cfg, args.checkpoint, args.score_thr, device=args.device)
+    if args.preview_model:
+        print(model.detector)
+        print('\n Please remove `--preview-model` to get the CAM.')
+        return
+
+    target_layers = []
+    for target_layer in args.target_layers:
+        try:
+            target_layers.append(eval(f'model.detector.{target_layer}'))
+        except Exception as e:
+            print(model.detector)
+            raise RuntimeError('layer does not exist', e)
+
+    det_cam = DetCam(
+        args.method,
+        model,
+        target_layers,
+        batch_size=args.batch,
+        reshape_transform=partial(
+            backbone_or_neck_reshape_transform,
+            max_shape=args.max_reshape_shape),
+        ablation_layer=AblationLayerBackboneOrNeck(),
+        ratio_channels_to_ablate=args.ratio_channels_to_ablate)
+    return model, det_cam
+
+
+def main():
+    args = parse_args()
+    cfg = Config.fromfile(args.config)
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+
+    model, det_cam = init_mode_cam(args, cfg)
+
+    images = args.img
+    if not isinstance(images, list):
+        images = [images]
+
+    for image_path in images:
+        image = cv2.imread(image_path)
+        model.set_input_data(image)
+        result = model()[0]
+
+        bboxes = result['bboxes'][..., :4]
+        scores = result['bboxes'][..., 4]
+        labels = result['labels']
+        segms = result['segms']
+        assert bboxes is not None and len(bboxes) > 0
+        if args.topk > 0:
+            idxs = np.argsort(-scores)
+            bboxes = bboxes[idxs[:args.topk]]
+            labels = labels[idxs[:args.topk]]
+            if segms is not None:
+                segms = segms[idxs[:args.topk]]
+        targets = [
+            DetBoxScoreTarget(bboxes=bboxes, labels=labels, segms=segms)
+        ]
+        grayscale_cam = det_cam(
+            image,
+            targets=targets,
+            aug_smooth=args.aug_smooth,
+            eigen_smooth=args.eigen_smooth)
+        image_with_bounding_boxes = det_cam.renormalize_cam_in_bounding_boxes(
+            image, bboxes, labels, grayscale_cam)
+
+        if args.out_dir:
+            mmcv.mkdir_or_exist(args.out_dir)
+            out_file = os.path.join(args.out_dir, os.path.basename(image_path))
+            mmcv.imwrite(image_with_bounding_boxes, out_file)
+        else:
+            cv2.namedWindow(os.path.basename(image_path), 0)
+            cv2.imshow(os.path.basename(image_path), image_with_bounding_boxes)
+            cv2.waitKey(0)
+
 
 if __name__ == '__main__':
-    config = '../configs/retinanet/retinanet_r50_fpn_1x_coco.py'
-    checkpoint = '/home/hha/Downloads/retinanet_r50_fpn_1x_coco_20200130-c2398f9e.pth'
-
-    device = 'cuda:0'
-    img_path = 'dog.jpg'  # 图片
-
-    cam_model = CAMWrapperModel(config, checkpoint, device=device)
-    img = cv2.imread(img_path)
-    cam_model.set_image(img)
-    result = cam_model()[0]
-
-    # 可视化分值最高的
-    bboxes = result['bboxes']
-    labels = result['labels']
-    index = bboxes[..., -1].argmax(-1)
-    bbox = bboxes[index, :4]  # xyxy
-    label = labels[index]  # xyxy
-
-    # 只可视化第一个框
-    targets = [DetBoxScoreTarget(bboxes=[bbox], labels=[label])]
-    target_layers = [cam_model.model.backbone.layer3]
-    det_cam = DetCam('AblationCAM', cam_model, target_layers,
-                     batch_size=1,
-                     ratio_channels_to_ablate=0.1)
-
-    grayscale_cam = det_cam(img, targets=targets)
-    image_with_bounding_boxes = det_cam.renormalize_cam_in_bounding_boxes(img, bbox, label, grayscale_cam)
-
-    cv2.namedWindow('image', 0)
-    cv2.imshow('image', image_with_bounding_boxes)
-    cv2.waitKey(0)
+    main()
--- mmdet/utils/det_cam.py
+++ mmdet/utils/det_cam.py
@@ -1,27 +1,34 @@
-import torch.nn as nn
-import torch
-from mmdet.apis import init_detector
+# Copyright (c) OpenMMLab. All rights reserved.
+import bisect
+
+import cv2
+import mmcv
 import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from mmcv.ops import RoIPool
+from mmcv.parallel import collate, scatter
+from pytorch_grad_cam import AblationCAM, AblationLayer, EigenCAM
+from pytorch_grad_cam.utils.image import scale_cam_image, show_cam_on_image
+
+from mmdet.apis import init_detector
 from mmdet.datasets import replace_ImageToTensor
 from mmdet.datasets.pipelines import Compose
-from mmcv.parallel import collate, scatter
-from mmcv.ops import RoIPool
-import cv2
-from pytorch_grad_cam import AblationCAM
-from pytorch_grad_cam.utils.image import show_cam_on_image, scale_cam_image
-import torchvision
 
 
 class CAMWrapperModel(nn.Module):
-    def __init__(self, cfg, checkpoint, cfg_options=None, device='cuda:0'):
+
+    def __init__(self, cfg, checkpoint, score_thr, device='cuda:0'):
         super().__init__()
         self.device = device
-        self.model = init_detector(cfg, checkpoint, device=device, cfg_options=cfg_options)
-        self.cfg = self.model.cfg
+        self.score_thr = score_thr
+        self.detector = init_detector(cfg, checkpoint, device=device)
+        self.cfg = self.detector.cfg
         self.input_data = None
         self.img = None
 
-    def set_image(self, img):
+    def set_input_data(self, img):
         self.img = img
         cfg = self.cfg.copy()
         # set loading pipeline type
@@ -32,14 +39,16 @@ class CAMWrapperModel(nn.Module):
         data = test_pipeline(data)
         data = collate([data], samples_per_gpu=1)
         # just get the actual data from DataContainer
-        data['img_metas'] = [img_metas.data[0] for img_metas in data['img_metas']]
+        data['img_metas'] = [
+            img_metas.data[0] for img_metas in data['img_metas']
+        ]
         data['img'] = [img.data[0] for img in data['img']]
 
-        if next(self.model.parameters()).is_cuda:
+        if next(self.detector.parameters()).is_cuda:
             # scatter to specified GPU
             data = scatter(data, [self.device])[0]
         else:
-            for m in self.model.modules():
+            for m in self.detector.modules():
                 assert not isinstance(
                     m, RoIPool
                 ), 'CPU inference with RoIPool is not supported currently.'
@@ -49,75 +58,203 @@ class CAMWrapperModel(nn.Module):
     @torch.no_grad()
     def __call__(self, *args, **kwargs):
         assert self.input_data is not None
-        results = self.model(return_loss=False, rescale=True, **self.input_data)[0]
-        bboxes = np.vstack(results)
+        results = self.detector(
+            return_loss=False, rescale=True, **self.input_data)[0]
+
+        if isinstance(results, tuple):
+            bbox_result, segm_result = results
+            if isinstance(segm_result, tuple):
+                segm_result = segm_result[0]  # ms rcnn
+        else:
+            bbox_result, segm_result = results, None
+
+        bboxes = np.vstack(bbox_result)
         labels = [
             np.full(bbox.shape[0], i, dtype=np.int32)
-            for i, bbox in enumerate(results)
+            for i, bbox in enumerate(bbox_result)
         ]
         labels = np.concatenate(labels)
-        return [{"bboxes": bboxes, 'labels': labels}]
+
+        segms = None
+        if segm_result is not None and len(labels) > 0:  # non empty
+            segms = mmcv.concat_list(segm_result)
+            if isinstance(segms[0], torch.Tensor):
+                segms = torch.stack(segms, dim=0).detach().cpu().numpy()
+            else:
+                segms = np.stack(segms, axis=0)
+
+        if self.score_thr > 0:
+            assert bboxes is not None and bboxes.shape[1] == 5
+            scores = bboxes[:, -1]
+            inds = scores > self.score_thr
+            bboxes = bboxes[inds, :]
+            labels = labels[inds]
+            if segms is not None:
+                segms = segms[inds, ...]
+        return [{"bboxes": bboxes, 'labels': labels, 'segms': segms}]
+
+
+def backbone_or_neck_reshape_transform(feats, max_shape=(20, 20)):
+    if isinstance(feats, torch.Tensor):
+        feats = [feats]
+
+    max_h = max([im.shape[-2] for im in feats])
+    max_w = max([im.shape[-1] for im in feats])
+    max_size = (min(max_h, max_shape[0]), min(max_w, max_shape[1]))
+
+    activations = []
+    for feat in feats:
+        activations.append(
+            torch.nn.functional.interpolate(
+                torch.abs(feat), max_size, mode='bilinear'))
+
+    activations = torch.cat(activations, axis=1)
+    return activations
+
+
+class AblationLayerBackboneOrNeck(AblationLayer):
+
+    def __init__(self):
+        super(AblationLayerBackboneOrNeck, self).__init__()
+        self.activations = None
+
+    def set_next_batch(self, input_batch_index, activations,
+                       num_channels_to_ablate):
+        """Extract the next batch member from activations, and repeat it
+        num_channels_to_ablate times."""
+        if isinstance(activations, torch.Tensor):
+            return super(AblationLayerBackboneOrNeck,
+                         self).set_next_batch(input_batch_index, activations,
+                                              num_channels_to_ablate)
+
+        self.activations = []
+        for activation in activations:
+            activation = activation[
+                input_batch_index, :, :, :].clone().unsqueeze(0)
+            self.activations.append(
+                activation.repeat(num_channels_to_ablate, 1, 1, 1))
+
+    def __call__(self, x):
+        """Go over the activation indices to be ablated, stored in
+        self.indices.
+
+        Map between every activation index to the tensor in the Ordered Dict
+        from the FPN layer.
+        """
+        result = self.activations
+
+        if isinstance(result, torch.Tensor):
+            return super(AblationLayerBackboneOrNeck, self).__call__(x)
+
+        channel_cumsum = np.cumsum([r.shape[1] for r in result])
+        num_channels_to_ablate = result[0].size(0)  # batch
+        for i in range(num_channels_to_ablate):
+            # 第几层的第几个通道
+            pyramid_layer = bisect.bisect_right(channel_cumsum,
+                                                self.indices[i])
+            if pyramid_layer > 0:
+                index_in_pyramid_layer = self.indices[i] - channel_cumsum[
+                    pyramid_layer - 1]
+            else:
+                index_in_pyramid_layer = self.indices[i]
+            result[pyramid_layer][i, index_in_pyramid_layer, :, :] = -1000
+        return result
 
 
 class DetCam:
-    def __init__(self, cam_method, model, target_layers, reshape_transform=None, batch_size=1,
+
+    def __init__(self,
+                 cam_method,
+                 model,
+                 target_layers,
+                 ablation_layer=None,
+                 reshape_transform=None,
+                 batch_size=1,
                  ratio_channels_to_ablate=0.1):
-        self.cam = AblationCAM(model,
-                               target_layers,
-                               use_cuda=True,
-                               reshape_transform=reshape_transform,
-                               batch_size=batch_size,
-                               ratio_channels_to_ablate=ratio_channels_to_ablate)
-        self.classes = model.model.CLASSES
+        if cam_method == 'ablationcam':
+            self.cam = AblationCAM(
+                model,
+                target_layers,
+                use_cuda=True if 'cuda' in model.device else False,
+                reshape_transform=reshape_transform,
+                batch_size=batch_size,
+                ablation_layer=ablation_layer,
+                ratio_channels_to_ablate=ratio_channels_to_ablate)
+        elif cam_method == 'eigencam':
+            self.cam = EigenCAM(
+                model,
+                target_layers,
+                use_cuda=True if 'cuda' in model.device else False,
+                reshape_transform=reshape_transform,
+            )
+        else:
+            raise NotImplementedError()
+
+        self.classes = model.detector.CLASSES
         self.COLORS = np.random.uniform(0, 255, size=(len(self.classes), 3))
 
-    def __call__(self, img, targets):
+    def __call__(self, img, targets, aug_smooth=False, eigen_smooth=False):
         img = torch.from_numpy(img)[None].permute(0, 3, 1, 2)
-        return self.cam(img, targets)[0, :]
+        return self.cam(img, targets, aug_smooth, eigen_smooth)[0, :]
 
-    def renormalize_cam_in_bounding_boxes(self, image, boxes, labels, grayscale_cam):
-        """Normalize the CAM to be in the range [0, 1]
-        inside every bounding boxes, and zero outside of the bounding boxes. """
+    def renormalize_cam_in_bounding_boxes(self, image, boxes, labels,
+                                          grayscale_cam):
+        """Normalize the CAM to be in the range [0, 1] inside every bounding
+        boxes, and zero outside of the bounding boxes."""
+        boxes = boxes.astype(np.int32)
+
         renormalized_cam = np.zeros(grayscale_cam.shape, dtype=np.float32)
-        images = []
 
-        x1, y1, x2, y2 = boxes.astype(np.int)
-        img = renormalized_cam * 0
-        img[y1:y2, x1:x2] = scale_cam_image(grayscale_cam[y1:y2, x1:x2].copy())
-        images.append(img)
+        images = []
+        for x1, y1, x2, y2 in boxes:
+            img = renormalized_cam * 0
+            img[y1:y2, x1:x2] = scale_cam_image(grayscale_cam[y1:y2,
+                                                              x1:x2].copy())
+            images.append(img)
 
         renormalized_cam = np.max(np.float32(images), axis=0)
         renormalized_cam = scale_cam_image(renormalized_cam)
-        eigencam_image_renormalized = show_cam_on_image(image / 255, renormalized_cam, use_rgb=False)
-        image_with_bounding_boxes = self._draw_boxes([boxes], [labels], [self.classes[labels]],
-                                                     eigencam_image_renormalized)
+        eigencam_image_renormalized = show_cam_on_image(
+            image / 255, renormalized_cam, use_rgb=False)
+        image_with_bounding_boxes = self._draw_boxes(
+            boxes, labels, eigencam_image_renormalized)
         return image_with_bounding_boxes
 
-    def _draw_boxes(self, boxes, labels, classes, image):
+    def _draw_boxes(self, boxes, labels, image):
         for i, box in enumerate(boxes):
-            color = self.COLORS[labels[i]]
-            cv2.rectangle(
+            label = labels[i]
+            color = self.COLORS[label]
+            cv2.rectangle(image, (int(box[0]), int(box[1])),
+                          (int(box[2]), int(box[3])), color, 2)
+            cv2.putText(
                 image,
-                (int(box[0]), int(box[1])),
-                (int(box[2]), int(box[3])),
-                color, 2
-            )
-            cv2.putText(image, classes[i], (int(box[0]), int(box[1] - 5)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2,
-                        lineType=cv2.LINE_AA)
+                self.classes[label], (int(box[0]), int(box[1] - 5)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.8,
+                color,
+                2,
+                lineType=cv2.LINE_AA)
         return image
 
 
 class DetBoxScoreTarget:
-    def __init__(self, bboxes, labels, match_iou_thr=0.5, device='cuda:0'):
-        self.focal_bboxes = bboxes
+
+    def __init__(self,
+                 bboxes,
+                 labels,
+                 segms=None,
+                 match_iou_thr=0.5,
+                 device='cuda:0'):
+        assert len(bboxes) == len(labels)
+        self.focal_bboxes = torch.from_numpy(bboxes).to(device=device)
         self.focal_labels = labels
-        assert isinstance(self.focal_bboxes, list)
-        assert isinstance(self.focal_labels, list)
-        assert len(self.focal_bboxes) == len(self.focal_labels)
-        self.focal_bboxes = [torch.tensor(focal_bbox[None, :], device=device)
-                             for focal_bbox in self.focal_bboxes]
+        if segms is not None:
+            assert len(bboxes) == len(segms)
+            self.focal_segms = torch.from_numpy(segms).to(device=device)
+        else:
+            self.focal_segms = [None] * len(labels)
         self.match_iou_thr = match_iou_thr
+
         self.device = device
 
     def __call__(self, results):
@@ -127,11 +264,21 @@ class DetBoxScoreTarget:
 
         pred_bboxes = torch.from_numpy(results["bboxes"]).to(self.device)
         pred_labels = results["labels"]
+        pred_segms = torch.from_numpy(results["segms"]).to(self.device)
 
-        for focal_box, focal_label in zip(self.focal_bboxes, self.focal_labels):
-            ious = torchvision.ops.box_iou(focal_box, pred_bboxes[..., :4])
+        for focal_box, focal_label, focal_segm in zip(self.focal_bboxes,
+                                                      self.focal_labels,
+                                                      self.focal_segms):
+            ious = torchvision.ops.box_iou(focal_box[None],
+                                           pred_bboxes[..., :4])
             index = ious.argmax()
-            if ious[0, index] > self.match_iou_thr and pred_labels[index] == focal_label:
+            if ious[0, index] > self.match_iou_thr and pred_labels[
+                    index] == focal_label:
+                # TODO: Adaptive adjustment of weights based on algorithms
                 score = ious[0, index] + pred_bboxes[..., 4][index]
-                output = output + score
+
+                if focal_segm is not None and pred_segms[index] is not None:
+                    segms_score = (focal_segm * pred_segms[index]).sum() / (
+                        focal_segm.sum() + pred_segms[index].sum() + 1e-7)
+                output = output + score + segms_score
         return output
